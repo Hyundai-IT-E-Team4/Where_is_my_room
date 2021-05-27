857428a685b29fa8abf4102ddb60406f42b87e1c
Hanseunghoon <3246902@naver.com>
2021-05-27 14:44:49 +0900

    feat: 헤더 세션 관리 기능 추가

--- src/main/java/com/org/team4/dto/LoginDTO.java
+++ src/main/java/com/org/team4/dto/LoginDTO.java
@@ -2,11 +2,8 @@ package com.org.team4.dto;
 
 import java.io.Serializable;
 
-<<<<<<< HEAD
 import org.apache.commons.codec.digest.DigestUtils;
 
-=======
->>>>>>> b012bfcf28fa8743dce867b31a84c67f736e98bc
 import lombok.Data;
 import lombok.ToString;
 
@@ -15,18 +12,11 @@ import lombok.ToString;
 public class LoginDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private String email;
 	private String password;
 
-<<<<<<< HEAD
 	public void setPassword(String password) {
 		this.password = DigestUtils.sha512Hex(password);
 	}
-=======
-//	public void setUsr_pw(String usr_pw) {
-//		this.usr_pw = DigestUtils.sha512Hex(usr_pw);
-//	}
-	
->>>>>>> b012bfcf28fa8743dce867b31a84c67f736e98bc
 }
